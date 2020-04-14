3b7d7f00f693c5332912a0c4f91975a849609315
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-14 17:13:40 -0400

    first commit

---
+++ src/completeInitilzation.java
@@ -0,0 +1,15 @@
+public class completeInitilzation {
+
+    public static void main(String[] args) {
+        int workOutDays = 10;
+        double health = 8.5;
+        boolean powerUp = true;
+
+        System.out.println ("Number of Days: " + workOutDays);
+        System.out.println ("Health Scale: " + health);
+        System.out.println ("Are you ready to power up? " + powerUp);
+        System.out.println ("Total Assessment: It has been 10 days since you've started working out.");
+        System.out.println ("You evaluate at " + health + " " + "out of " + workOutDays + " on the health scale");
+        System.out.println ("It is true that you are ready to power up your workout!");
+    }
+}
